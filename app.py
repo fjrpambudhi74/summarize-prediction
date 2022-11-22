3b1bccd811001efcae4656d850d65395014f8d72
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2022-11-22 10:56:08 +0700

    add sidebar menu

--- app.py
+++ app.py
@@ -11,6 +11,13 @@ import streamlit as st
 def main():
 
   st.title("Summary and Entity Checker")
+  activities = ["Summarize", "NER Checker", "NER for URL"]
+  choice = st.sidebar.selectbox("Select Activity", activities)
+
+  if choice == "Summarize":
+    st.subheader("Summary with NLP")
+    raw_text = st.text_area("Enter text here")
+    st.write(raw_text)
 
 
 if __name__ == '__main__':
