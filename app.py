36941abaebf9dee76adc45d4fe1af80d4d4aec78
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2022-11-22 10:54:33 +0700

    setup streamlit

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+#NLP
+
+#Summary Packages
+
+#Function Summarize
+
+#Web Scrapping
+
+def main():
+
+  st.title("Summary and Entity Checker")
+
+
+if __name__ == '__main__':
+  main()
