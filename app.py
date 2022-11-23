39f94a4f8e44099ee588b81d3fea3c1cf403e25b
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2022-11-23 10:52:54 +0700

    implement entity checker

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
 #NLP
+import spacy
+from spacy import displacy
+nlp = spacy.load("en_core_web_sm")
+HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem">{}</div>"""
 
 #Summary Packages
 from gensim.summarization import summarize
@@ -16,6 +20,11 @@ def sumy_summarizer(docx):
 	summary_list = [str(sentence) for sentence in summary]
 	result = ' '.join(summary_list)
 	return result
+
+#NLP
+# @st.cache(allow_output_mutation=True)
+def analyze_text(text):
+	return nlp(text)
 
 
 #Web Scrapping
@@ -38,6 +47,17 @@ def main():
         summary_result = sumy_summarizer(raw_text)
 
       st.write(summary_result)
+
+  if choice == 'NER Checker':
+    st.subheader("Entity Recognition with Spacy")
+    raw_text = st.text_area("Enter Text Here", "Type Here")
+
+    if st.button("Analyze"):
+    #NLP
+        docx = analyze_text(raw_text)
+        html = displacy.render(docx,style="ent")
+        html = html.replace("\n\n","\n")
+        st.write(HTML_WRAPPER.format(html),unsafe_allow_html=True)
 
 
 if __name__ == '__main__':
