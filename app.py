81ec8ec0ff1fdbe239ea5b5241e2014e04a27fe6
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2022-11-23 11:22:55 +0700

    implement analyze text from URL

--- app.py
+++ app.py
@@ -6,11 +6,16 @@ from spacy import displacy
 nlp = spacy.load("en_core_web_sm")
 HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem">{}</div>"""
 
-#Summary Packages
+##Summary Packages
+# Gensim Packages
 from gensim.summarization import summarize
+# Sumy Packages
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
+# URL Summarize Packages
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
 #Function Prediction Summary
 def sumy_summarizer(docx):
@@ -21,19 +26,28 @@ def sumy_summarizer(docx):
 	result = ' '.join(summary_list)
 	return result
 
-#NLP
+#Function Analyze entity text
 # @st.cache(allow_output_mutation=True)
 def analyze_text(text):
 	return nlp(text)
+
+# Function Fetch text from URL
+@st.cache
+def get_text(raw_url):
+	page = urlopen(raw_url)
+	soup = BeautifulSoup(page)
+	fetched_text = ' '.join(map(lambda p: p.text, soup.find_all('p')))
+	return fetched_text
 
 
 #Web Scrapping
 def main():
 
   st.title("Summary and Entity Checker")
-  activities = ["Summarize", "NER Checker", "NER for URL"]
+  activities = ["Summarize", "NER Checker", "NER URL"]
   choice = st.sidebar.selectbox("Select Activity", activities)
 
+  # Summarize text
   if choice == "Summarize":
     st.subheader("Summary with NLP")
     raw_text = st.text_area("Enter text here")
@@ -48,6 +62,7 @@ def main():
 
       st.write(summary_result)
 
+  #Entity check from text
   if choice == 'NER Checker':
     st.subheader("Entity Recognition with Spacy")
     raw_text = st.text_area("Enter Text Here", "Type Here")
@@ -59,6 +74,26 @@ def main():
         html = html.replace("\n\n","\n")
         st.write(HTML_WRAPPER.format(html),unsafe_allow_html=True)
 
+  # Summarize text from URL
+  if choice == 'NER URL':
+    st.subheader("Entity Recognition with Spacy")
+    raw_url = st.text_input("Enter URL Here", "Type here")
+    text_preview_length = st.slider("Length to Preview", 50, 100)
+    if st.button("Analyze"):
+      if raw_url != "Type here":
+        result = get_text(raw_url)
+        len_of_full_text = len(result)
+        len_of_short_text = round(len(result)/text_preview_length)
+        st.success("Full Text Length = {}".format(len_of_full_text))
+        st.info(result[:len_of_short_text])
+        summarized_docx = sumy_summarizer(result)
+        docx = analyze_text(summarized_docx)
+        html = displacy.render(docx, style="ent")
+        html = html.replace("\n\n", "\n")
+        st.success("Summarize Text Length = {}".format(len_of_short_text))
+        st.write(html,unsafe_allow_html=True)
 
+
+# Main Function
 if __name__ == '__main__':
   main()
